b648781e8314b6c06c61b268c23174242fb63972
khadija <khadijajjop2@gmail.com>
2026-07-14 20:47:09 +0000

    Creation des routes

--- app/core/routes.php
+++ app/core/routes.php
@@ -1,1 +1,39 @@
 <?php
+
+$routes =[
+    'GET'=>[],
+    'POST'=>[]
+];
+
+function getRoute(string $path,string $functionName){
+    global $routes;
+    $functionName = $routes['GET'][$path];
+}
+
+function postRoute(string $path,string $functionName){
+    global $routes;
+    $functionName = $routes['POST'][$path];
+}
+
+function cheminRoutage(string $uri,string $method){
+    global $routes;
+    $path = rtrim($uri,'/');
+    if(empty($path)){
+        $path = '/';
+    }
+
+    $functionName = $routes[$method][$path]?? null;
+    if($functionName==null){
+        http_response_code(404);
+        echo "<h1>Erreur 404</h1><p>La page demandée n'existe pas </p>";
+        exit;
+    }
+
+    if (function_exists($functionName)) {
+        $functionName();
+    } else {
+        http_response_code(500);
+        echo "Erreur : La fonction PHP  n'existe pas.";
+    }
+
+}
